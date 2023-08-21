38c4e204b3623b83ed548605781b229ffa4fa704
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-08-21 18:36:44 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require("inquirer");
 
+const connection = require('./db');
+
+function mainMenu() {
+    inquirer
+        .prompt ([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View employees',
+                    'Add employee',
+                    'Exit'
+                ]
+            }
+        ])
+
+        .then(answer => {
+            switch (answer.action) {
+                case 'View all employees':
+                    break;
+                case 'Add employee':
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        })
+}
+
+mainMenu();
